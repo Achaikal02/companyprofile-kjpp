a2f4ce3dd10ad5d1f74e2b25b55b1fff94d5801b
Achaikal02 <haikalflashbanjarnegara@gmail.com>
2026-01-06 08:47:26 +0700

    fix team

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -4,20 +4,76 @@ export const teamData = [
     role: 'Managing Partner',
     license: 'No. P-1.09.00155',
     email: 'a.suherman@kjppasr.co.id',
-    photo: 'dummy.png'
+    photo: 'ayon.png'
   },
   {
     name: 'Mahrizal Ependi, SE., MAPPI (Cert)',
     role: 'Partner',
     license: 'No. P-1.12.00336',
-    email: 'penilai@kjppasr.co.id',
-    photo: 'dummy.png'
+    email: 'mahrizal.nas@gmail.com',
+    photo: 'MahrizalEpendi.png'
   },
   {
     name: 'Dwi Rohmawati, SP., MAPPI (Cert)',
     role: 'Partner',
     license: 'No. P-1.12.00337',
-    email: 'penilai@kjppasr.co.id',
-    photo: 'dummy.png'
+    email: 'watiqq@yahoo.com',
+    photo: 'DwiRohmawati.png'
+  },
+  {
+    name: 'Ir. Rosikhun Fadlol, MAPPI (Cert.)',
+    role: 'Partner',
+    license: 'No. P-1.10.00294',
+    email: 'rosikhun.fdl@kjppasr.co.id',
+    photo: 'RosikhunFadlol.png'
+  },
+  {
+    name: 'Kiryanto, S.Si., MAPPI (Cert.)',
+    role: 'Partner',
+    license: 'No. P-1.18.00513',
+    email: 'kiryanto.anto22@gmail.com',
+    photo: 'Kiryanto.png'
+  },
+  {
+    name: 'Suryono',
+    role: 'Partner',
+    license: '-',
+    email: 'suryono@kjppasr.co.id',
+    photo: 'Suryono.png'
+  },
+  {
+    name: 'Wawan Trisnawan, ST. MT., MAPPI (Cert.)',
+    role: 'Partner',
+    license: 'No. PS-1.16.00092',
+    email: 'wtrisnawan@gmail.com',
+    photo: 'WawanTrisnawan.png'
+  },
+  {
+    name: 'Taufan Bachtiar, S.Kom., MAPPI (Cert.)',
+    role: 'Partner',
+    license: 'No. PS-1.15.00027',
+    email: 'beetaufanbe@gmail.com',
+    photo: 'TaufanBachtiar.png'
+  },
+  {
+    name: 'Achmad Fa’izin, S.T., M.M., MAPPI (Cert.)',
+    role: 'Partner',
+    license: 'No. P-1.13.00359',
+    email: 'a.faizin@kjppasr.co.id',
+    photo: 'AchmadFaizin.png'
+  },
+  {
+    name: 'Nova Naldi Susanto Uga, S.T., MAPPI (Cert.)',
+    role: 'Partner',
+    license: 'No. PS-1.19.00269',
+    email: 'novanaldi86@gmail.com',
+    photo: 'NovaNaldi.png'
+  },
+  {
+    name: 'Yusrizal, S.T., MAPPI (Cert.)',
+    role: 'Partner',
+    license: 'No. PS-1.19.00248',
+    email: 'yusrizal02515@gmail.com',
+    photo: 'Yusrizal.png'
   }
 ]
